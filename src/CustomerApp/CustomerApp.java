5ebf84b6b2140f1dfb016bbc29d539a15c26b1bb
dosora1 <61692372+dosora1@users.noreply.github.com>
2021-02-02 17:17:32 +0900

    Create CustomerApp.java

---
+++ src/CustomerApp/CustomerApp.java
@@ -0,0 +1,44 @@
+package CustomerApp;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class CustomerApp {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CustomerApp window = new CustomerApp();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public CustomerApp() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+	}
+
+}
